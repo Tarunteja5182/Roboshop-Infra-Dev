8fb51a2ec46ef699e5c4747e7c7a3350311089a9
Tarunteja <taruntejamahanthi@gmail.com>
2026-03-09 15:23:59 +0530

    chages ssm value

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -2,7 +2,7 @@ locals{
     ami_id= data.aws_ami.joindevops.id
     project = "roboshop"
     environment = "dev"
-    mongo_subnet_id = split(",",data.aws_ssm_parameter.mongo_subnet_id)[0]
+    mongo_subnet_id = split(",",data.aws_ssm_parameter.mongo_subnet_id.value)[0]
     mongo_sg_id = data.aws_ssm_parameter.mongo_sg_id.value
     rm_user= "ec2-user"
     rm_pwd="DevOps321"
