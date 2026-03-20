f366dc2012bf8c668ab5b16cb157bb3e28aa4eae
Tarunteja <taruntejamahanthi@gmail.com>
2026-03-20 19:39:31 +0530

    changes to source

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component"{
     for_each = var.components
-    source = "../Roboshop-infra-component"
+    source = "../../Roboshop-infra-component"
     component = each.key
     rule_priority = each.value.rule_priority
 }
