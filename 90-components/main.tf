0c5aa8d037739cf9fd6910a6147195acbff54dde
Tarunteja <taruntejamahanthi@gmail.com>
2026-03-20 19:25:46 +0530

    initial component user code

---
+++ 90-components/main.tf
@@ -0,0 +1,6 @@
+module "component"{
+    for_each = var.components
+    source = "../Roboshop-infra-component"
+    component = each.key
+    rule_priority = each.value.rule_priority
+}
---
+++ 90-components/variables.tf
@@ -0,0 +1,22 @@
+variable "components"{
+    default = {
+        catalogue = {
+            rule_priority= 10
+        }
+        cart = {
+            rule_priority= 20
+        }
+        user = {
+            rule_priority= 30
+        }
+        payment = {
+            rule_priority= 40
+        }
+        shipping = {
+            rule_priority= 50
+        }
+        frontend = {
+            rule_priority= 10
+        }
+    }
+}
